23780033113bffbdd41343a40c708164340e93cc
System Administrator <root@Neelams-MacBook-Pro.local>
2017-07-28 23:57:25 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket=io();
 
 socket.on('connect',function(){
   console.log('Connected to server');
-
-  socket.emit('createMessage',{
-    to: 'Raunak',
-    text: 'I am a bit busy today'
-  });
 });
 
 socket.on('disconnect',function(){
--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New User Connected');
 
-  socket.emit('newMessage',{
-    from: 'Abhi',
-    text: 'Can we meet up?',
-    createdAt: new Date().getTime()
-  });
 
   socket.on('createMessage',(msg)=>{
     console.log('Message: ',msg);
-  })
+    io.emit('newMessage',{
+      from: msg.from,
+      text: msg.text,
+      createdAt: new Date().getTime()
+    });
+  });
 
   socket.on('disconnect',()=>{
     console.log('User was disconnected');
