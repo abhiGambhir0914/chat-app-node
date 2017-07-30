2ad5b50a2bfbed90abe94955f581fe74e6b1ec48
System Administrator <root@Neelams-MacBook-Pro.local>
2017-07-30 23:00:55 +0530

    Add forms for msges and show incoming for browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,22 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage', function(msg){
   console.log('New message on chat app',msg);
+
+  var li=jQuery('<li></li>');
+  li.text(`${msg.from}: ${msg.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+
+jQuery('#msg-form').on('submit',function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage',{
+    from:'user',
+    text: jQuery('[name=msg-box]').val()
+  },function(){
+
+  });
+
 });
